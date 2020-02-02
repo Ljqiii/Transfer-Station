103ec5f7bcb49ba2a2812c92c0bfbf07353f03bb
reoh <1750913582@qq.com>
2020-02-02 16:37:27 +0800

    new commit

--- untitled1/css/enroll.js
+++
@@ -1,7 +0,0 @@
-var enroll= document.getElementById("enroll");
-enroll.onclick= function (e) {
-    e.preventDefault();
-    enroll.innerHTML= "I could not awaken";
-    enroll.style.background= "#27cb8b";
-    enroll.style.border= "#27cb8b";
-}
